7ebcae75e86723015a472537318be41c3805eda7
kpsshankar <kpsshankar@yahoo.co.in>
2024-04-16 20:46:07 +0530

    Update Explicitwait_Expectedcontions_Task-25.py

--- Explicitwait_Expectedcontions_Task-25.py
+++ Explicitwait_Expectedcontions_Task-25.py
@@ -25,73 +25,76 @@ class Filters:
 
 
     def boot(self):
+       self.wait = WebDriverWait(self.driver, 10)
        self.driver.get(self.url)
        self.driver.maximize_window()
        #self.wait.until(ec.url_to_be(self.url))
-       self.wait = WebDriverWait(self.driver, 10)
+
        
     def fillName(self,Name):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[1]/div[1]/label/span[1]/div"
+       self.wait = WebDriverWait(self.driver, 10)
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[1]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Name)
        sleep(5)
        
-    def fillBirthdate(self,Birthsdate):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[2]/div[1]/label/span[1]/div/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[2]/div[1]/label/span[1]/div"
+    def fillBirthdate(self,Birthdate):
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[2]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Birthdate)
        sleep(5)
        
     def fillBirthday(self,Birthday):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[3]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[3]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Birthday)
        sleep(5)
        
     def fillAwardsrecognition(self,Awardsrecognition):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[4]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[4]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Awardsrecognition)
        sleep(5)
        
     def fillPagetopics(self,Pagetopics):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[5]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[5]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Pagetopics)
        sleep(5)
        
     def fillDeathdate(self,Deathdate):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[6]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[6]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Deathdate)
        sleep(5)
        
        
     def fillGenderidentity(self,Genderidentity):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[7]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[7]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Genderidentity)
        sleep(5)
        
        
     def fillCredits(self,Credits):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[8]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[8]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Credits)
        sleep(5)
        
        
     def fillAdultnames(self,Adultnames):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[9]/div[1]/label/span[1]/div"
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/section/div/div[9]/div[1]/label"
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
        element.send_keys(Adultnames)
        sleep(5)
        
        
-    def fillSearchfilters(self,Searchfilters):
-       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[2]/div[1]/div/text()"
+    def clickSeeresults(self,Seeresults):
+       fullXPath = "/html/body/div[2]/main/div[2]/div[3]/section/section/div/section/section/div[2]/div/section/div[1]/button".click()
        element = self.driver.find_element(by=By.XPATH, value=fullXPath)
-       element.send_keys(Searchfilters)
+       #element.send_keys(Searchfilters)
+       element.click(Seeresults)
        sleep(5)
        
     
@@ -100,13 +103,13 @@ class Filters:
 obj = Filters("https://www.imdb.com/search/name//")
 obj.boot()
 obj.fillName("Tharani")
-obj.Birthdate("05-05-1990")
-obj.Birthday("10-10")
-obj.Awardsrecognition("Best Acterss-Nominated")
-obj.Pagetopics("Biography")
-obj.Deathdate("10-10-2090")
-obj.Genderidentity("Male")
-obj.Credits("100")
-obj.Adultnames("Include")
-obj.Searchfilters()
+obj.fillBirthdate("05-05-1990")
+obj.fillBirthday("10-10")
+obj.fillAwardsrecognition("Best Acterss-Nominated")
+obj.fillPagetopics("Biography")
+obj.fillDeathdate("10-10-2090")
+obj.fillGenderidentity("Male")
+obj.fillCredits("100")
+obj.fillAdultnames("Include")
+obj.clickSeeresults(Seeresults)
 
